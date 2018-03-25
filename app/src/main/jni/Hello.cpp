97391f807ba4e86d3cc1e5c415e761ec9be15e9f
wangzh <wangzh526@126.com>
2018-03-25 21:31:10 +0800

    生成头文件和调用

---
+++ app/src/main/jni/Hello.cpp
@@ -0,0 +1,11 @@
+//
+// Created by wang on 2018/3/25.
+//
+
+include "com_wang_zh_jnidemo_Hello.h"
+
+JNIEXPORT jstring JNICALL Java_com_wang_zh_jnidemo_Hello_sayHello
+  (JNIEnv env, jclass jclass1) {
+
+  return env->
+  }
