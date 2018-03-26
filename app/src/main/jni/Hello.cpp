78624211c3d29add5ea1ff53002f820950f93267
wangzhenhua <wangzh526@126.com>
2018-03-26 15:29:13 +0800

    add some code

--- app/src/main/jni/Hello.cpp
+++ app/src/main/jni/Hello.cpp
@@ -7,5 +7,5 @@ include "com_wang_zh_jnidemo_Hello.h"
 JNIEXPORT jstring JNICALL Java_com_wang_zh_jnidemo_Hello_sayHello
   (JNIEnv env, jclass jclass1) {
 
-  return env->
+  return env->NewStringUTF("hello from C");
   }
